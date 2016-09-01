cf93d93436968df3f593be0a59f3b7eba494ae3b
Nesada Koca <nesadakoca@Artans-MacBook-Pro.local>
2016-08-31 21:52:59 -0400

    added src code

--- src/nyc/c4q/Main.java
+++ src/nyc/c4q/Main.java
@@ -1,24 +1,47 @@
 package nyc.c4q;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Hangman hangman = new Hangman();
-        while (hangman.getMisses() < 5){
-            hangman.printCurrentWord();
-            hangman.promptPlayer("Enter a letter: ");
-            hangman.readLetter();
-            hangman.checkLetter();
-            if(hangman.guessedSuccessfully()){
-                break;
+        boolean play = true;
+
+        while (play) {
+            Hangman hangman = new Hangman();
+            while (hangman.getMisses() < 5) {
+                hangman.printCurrentWord();
+                hangman.promptPlayer("Enter a letter: ");
+                hangman.readLetter();
+                hangman.checkLetter();
+                if (hangman.guessedSuccessfully()) {
+                    break;
+                }
+                System.out.println(Drawing.get(hangman.getMisses()));
+                System.out.println("Misses -> " + hangman.getMisses());
             }
-            System.out.println(Drawing.get(hangman.getMisses()));
-            System.out.println("Misses -> " +hangman.getMisses());
-        }
-        if(hangman.guessedSuccessfully()){
-            System.out.println("Success");
-        } else {
-            System.out.println("The answer was " + hangman.getSecretWord());
+            if (hangman.guessedSuccessfully()) {
+                System.out.println("Success");
+            } else {
+                System.out.println("The answer was " + hangman.getSecretWord());
+            }
+
+            System.out.println("Play again? (yes/no)");
+
+            boolean validAnswer = false;
+            do {
+                Scanner in = new Scanner(System.in);
+                String answer = in.next();
+                answer = answer.toLowerCase();
+                if (answer.equals("yes") || answer.equals("y")) {
+                    play = true;
+                    validAnswer = true;
+                } else if (answer.equals("no")|| answer.equals("n")){
+                    play = false;
+                    validAnswer = true;
+                } else {
+                    System.out.println("Please answer with yes or no. Thank You!");
+                }
+            }while (validAnswer == false);
         }
     }
 }
